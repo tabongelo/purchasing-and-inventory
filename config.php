7117533cc2358fe83227110253b4f37f9c61e997
OGMac12 <malvarmac6@gmail.com>
2023-05-14 13:52:32 +0800

    files commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','purchase') or die('connection failed');
+
+?>
---
+++ footer.php
@@ -0,0 +1,14 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+   </div>
+
+</section>
